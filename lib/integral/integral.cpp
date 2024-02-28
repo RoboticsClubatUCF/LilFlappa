7fa6e7ea29a97f46e2633811cd70d171a3756c2a
biggiePete <peter.cross222@gmail.com>
2024-02-27 19:13:03 -0500

    pico

--- lib/integral/integral.cpp
+++ lib/integral/integral.cpp
@@ -1,3 +1,14 @@
+/**
+ * @file integral.cpp
+ * @author Peter Cross (peter.cross222@gmail.com)
+ * @brief defines basic integration functions
+ * @version 0.1
+ * @date 2024-02-27
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
 #include "integral.h"
 
 double integrate(double integrand, double deltaTime)
--- lib/integral/integral.h
+++ lib/integral/integral.h
@@ -1,3 +1,14 @@
+/**
+ * @file integral.cpp
+ * @author Peter Cross (peter.cross222@gmail.com)
+ * @brief defines basic integration functions
+ * @version 0.1
+ * @date 2024-02-27
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
 #ifndef C32AA91C_8631_446A_B08C_9575A8512ACE
 #define C32AA91C_8631_446A_B08C_9575A8512ACE
 double integrate(double integrand, double deltaTime);
