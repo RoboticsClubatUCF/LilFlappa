68f7a17c6207965505947e6b3dcdcfab3ee7216c
biggiePete <peter.cross222@gmail.com>
2024-02-27 19:32:53 -0500

    datatypes

--- src/datatypes.h
+++ src/datatypes.h
@@ -2,21 +2,23 @@
 #define E0F0B197_E145_4160_AC80_450265C146BE
 typedef struct
 {
-  double ddx; // acceleration in the x direction
-  double ddy; // acceleration in the y direction
-  double ddz; // acceleration in the z direction
+  double x;
+  double y;
+  double z;
+} vector3_cartesian;
+typedef struct
+{
+  double x;
+  double y;
+  double z;
+} vector3_polar;
+typedef struct
+{
+  vector3_cartesian acceleration; // accleration vector
+  vector3_cartesian velocity;
 
-  double dx; // velocity in the x direction
-  double dy; // velocity in the y direction
-  double dz; // velocity in the z direction
-
-  double dpitch; // change in rotation of the pitch
-  double droll;  // change in rotation of the roll
-  double dyaw;   // change in rotation of the yaw
-
-  double pitch; // pitch
-  double roll;  // roll
-  double yaw;   // yaw
+  vector3_polar vdegs;
+  vector3_polar degs;
 
 } five_axis_t;
 
